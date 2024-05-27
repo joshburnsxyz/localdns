4787a9a87874527a386462dae91dcf5f7813da8c
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2024-05-27 03:00:59 +0000

    function to read in a CSV file and generate a records map

--- pkg/record/record.go
+++ pkg/record/record.go
@@ -2,6 +2,9 @@ package record
 
 import (
   "errors"
+  "fmt"
+  "encoding/csv"
+  "os"
 )
 
 type Records map[string]string
@@ -16,4 +19,39 @@ func (r *Records) Add(domain string, ipaddr string) error {
   // Populate records map
   r[domain] = ipaddr
   return nil
+}
+
+func NewRecordsFromCSV(csvfilepath string) (*Records, error) {
+
+  f, err := os.Open(csvfilepath)
+  if err != nil {
+    return nil, errors.New(fmt.Sprintf("Error opening CSV file: %s", err))
+  }
+  defer f.Close()
+  csvReader := csv.NewReader(f)
+  
+  data, err := csvReader.ReadAll()
+  if err != nil {
+    return nil, errors.New(fmt.Sprintf("Error reading CSV file: %s", err))
+  }
+
+  var records = make(Records)
+  for i, line := range data {
+    var domainFromCsv string
+    var ipv4FromCsv string
+    if i > 0 {
+      for j, field := range line {
+        if j == 0 {
+          domainFromCsv = field
+        } else if j== 1 {
+          ipv4FromCsv = field
+        }
+      }
+
+      // Create new record on the map
+      records.Add(domainFromCsv, ipv4FromCsv)
+    }
+  }
+
+  return &records,nil
 }
