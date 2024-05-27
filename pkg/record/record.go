1dc2989ecc61188053548973e71e099c5ac57e10
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2024-05-27 02:50:17 +0000

    return an error from Add()

--- pkg/record/record.go
+++ pkg/record/record.go
@@ -1,10 +1,19 @@
 package record
 
+import (
+  "errors"
+)
+
 type Records map[string]string
 
 func (r *Records) Add(domain string, ipaddr string) error {
-  // TODO: check if domain is already present and 
-  // return an error if so.
+  for key, val := range r {
+    if key == domain {
+      return errors.New("Domain record already exists")
+    }
+  }
+
+  // Populate records map
   r[domain] = ipaddr
   return nil
 }
