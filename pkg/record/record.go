2b11602c50effc23f07fdd4481bf6f6039d6cc6d
Josh Burns <joshyburnss@gmail.com>
2024-05-28 17:18:25 +1000

    bind Add() to Record type directly, not a pointer

--- pkg/record/record.go
+++ pkg/record/record.go
@@ -9,8 +9,8 @@ import (
 
 type Records map[string]string
 
-func (r *Records) Add(domain string, ipaddr string) error {
-  for key, val := range r {
+func (r Records) Add(domain string, ipaddr string) error {
+  for key, _ := range r {
     if key == domain {
       return errors.New("Domain record already exists")
     }
